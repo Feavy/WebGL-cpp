d62345b768b77d3c95aeadca0a9a9932d9ff54ff
Feavy <quentin071100@live.fr>
2021-06-06 21:05:41 +0200

    Multicolor square

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include <memory>
@@ -26,21 +27,22 @@ void opengl_init() {
     // Exemple : varying highp vec4 vertexColor;
 
     const char *vertexShaderSource = "attribute vec4 vPosition;                 \n"
+                                     "attribute vec3 aColor;                 \n"
                                      "varying mediump vec4 vertexColor;         \n"
                                      "void main()                               \n"
                                      "{                                         \n"
                                      "   gl_Position = vPosition;               \n"
-                                     "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+                                     "   vertexColor = vec4(aColor, 1.0);\n"
                                      "}                                         \n";
 
     unsigned int vertexShader = loadShader(GL_VERTEX_SHADER, vertexShaderSource);
 
     const char *fragmentShaderSource = "precision mediump float;                \n"
                                        "varying mediump vec4 vertexColor;       \n"
-                                       "uniform vec4 ourColor;                  \n"
+                                       //    "uniform vec4 ourColor;                  \n"
                                        "void main()                             \n"
                                        "{                                       \n"
-                                       "  gl_FragColor = ourColor;              \n"
+                                       "  gl_FragColor = vertexColor;           \n"
                                        "}                                       \n";
 
     unsigned int fragmentShader = loadShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -64,9 +66,7 @@ void opengl_init() {
             var dt = timestamp - lastTime;
             lastTime = timestamp;
             Module._newFrame(dt);
-            if (i++ < 60 * 10) {
-                requestAnimationFrame(step);
-            }
+            requestAnimationFrame(step);
         }
         function prestep(timestamp) {
             lastTime = timestamp;
@@ -93,10 +93,16 @@ void opengl_draw(float dt) {
     float transformVal = (sin(elapsed / 1000.f) / 2.0f) + 0.5f;
 
     float vertices[] = {
-        -0.5f, 0.5f, 0.0f,                   // top left
-        transformVal, transformVal, 0.0f,    // top right
-        0.5f, -0.5f, 0.0f,                   // bottom right
-        -transformVal, -transformVal, 0.0f}; // bottom left
+        // position  // color
+        -0.5f, 0.5f, 0.0f, /**/ 0.f, 1-transformVal, transformVal,                   // top left
+        transformVal, transformVal, 0.0f, /**/ 0.f, transformVal, 1-transformVal,    // top right
+        0.5f, -0.5f, 0.0f, /**/ transformVal, 0.f, 0.f,                   // bottom right
+        -transformVal, -transformVal, 0.0f, /**/ 1-transformVal, transformVal, transformVal}; // bottom left
+    /*
+    Autre possibilité : faire un second tableau pour les couleurs
+    et utiliser un système analogue au tableau vertices pour y accéder depuis le shader
+    https://github.com/bonigarcia/webgl-examples/blob/master/basic_concepts/colored-triangle.html
+    */
 
     unsigned short indices[] = {
         0, 1, 3,
@@ -126,9 +132,13 @@ void opengl_draw(float dt) {
 
     // Note : VAO uniquement dispo en WebGL2 (77% de coverage browers)
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
-    // glVertexAttribPointer(0, 3, GL_FLOAT, 0, 0, 0);
+    // Ou utiliser getAttribLocation
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // glVertexAttribPointer(0, 3, GL_FLOAT, 0, 0, 0);
 
     // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
